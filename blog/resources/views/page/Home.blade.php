7cd88d0bfcd44c8dca9de96dddb46bec2aac3a5a
Muhammad Aqila <aqiladaffa92@gmail.com>
2021-07-28 23:42:00 +0700

    laravel blade template

---
+++ blog/resources/views/page/Home.blade.php
@@ -0,0 +1,24 @@
+<!-- 
+    Muhammad Aqila Karindra Daffa
+    aqiladaffa92@gmail.com
+-->
+@extends('layout/master')
+@section('title')
+    <h1>SanberBook</h1>
+    @endsection
+    @section('content')
+    <h2>Social Media Developer Santai Berkualitas</h2>
+    <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+    <h3>Benefit Join di SanberBook</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama developer</li>
+        <li>Sharing knowledge dari para mastah Sanber</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3>Cara Bergabung ke SanberBook</h3>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
+        <li>Selesai!</li>
+    </ol>
+    @endsection
